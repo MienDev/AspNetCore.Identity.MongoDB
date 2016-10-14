320ffdaa30c4e5ac5f58342111a2e3b4af53677f
Mien Wu <mien@dreamxo.net>
2016-10-14 19:28:51 +0800

    Initial Tests.

---
+++ tests/MienDev.AspNetCore.Identity.MongoDB.IntegratedTests/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MienDev.AspNetCore.Identity.MongoDB.IntegratedTests.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
---
+++ tests/MienDev.AspNetCore.Identity.MongoDB.IntegratedTests/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MienDev.AspNetCore.Identity.MongoDB.IntegratedTests.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
